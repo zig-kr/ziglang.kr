22c8116de26c78a0a72d1cd1287b9c4c2b9ef271
Heewon Cho <heewon6752@gmail.com>
2023-08-27 19:20:44 +0900

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<div class="flex justify-center items-center h-[100dvh]">
+    <iframe
+        class="block mx-auto"
+        title="discord"
+        src="https://discord.com/widget?id=1141415718655631440&theme=dark"
+        width="500"
+        height="700"
+        allowtransparency={true}
+        frameborder="0"
+        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+    />
+</div>
